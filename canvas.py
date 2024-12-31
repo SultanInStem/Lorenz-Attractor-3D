61f46d266a28faf1453af23e474f5e18959cb960
= <sultaninstem004@gmail.com>
2024-12-30 20:32:10 -0800

    coords

--- canvas.py
+++ canvas.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+from globals import to_math_coords, to_screen_coords
 class Canvas: 
     def __init__(self, width, height): 
         pygame.init()
@@ -8,6 +8,7 @@ class Canvas:
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.screen_size = self.screen.get_size()
     def handle_events(self): 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
@@ -18,6 +19,7 @@ class Canvas:
     def render(self):
         self.screen.fill((0,0,0))
 
+        pygame.draw.circle(self.screen, (255,255,255), to_screen_coords((-100,-100),self.screen_size), 20, 0)
         pygame.display.flip()
         self.clock.tick(30)
     def run(self): 
--- globals.py
+++ globals.py
@@ -4,4 +4,6 @@ def to_math_coords(point, screen_size):
     return (math_x, math_y)
 
 def to_screen_coords(point, screen_size): 
-    pass 
+    screen_x = point[0] + (screen_size[0] // 2)
+    screen_y = (screen_size[1] // 2) - point[1]
+    return (screen_x, screen_y)
